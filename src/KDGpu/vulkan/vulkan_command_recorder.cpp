ba4973f0481818bbae2231825ca735bd5c564f9c
houmain <houmain@posteo.net>
2025-01-08 12:35:33 +0100

    Improved generateMipMaps

--- src/KDGpu/vulkan/vulkan_command_recorder.cpp
+++ src/KDGpu/vulkan/vulkan_command_recorder.cpp
@@ -550,16 +550,82 @@ void VulkanCommandRecorder::generateMipMaps(const GenerateMipMapsOptions &option
                 },
         });
 
-    for (uint32_t mipLevel = 1; mipLevel < options.mipLevels; ++mipLevel) {
+    // Transition all other miplevels to TransferDstOptimal
+    textureMemoryBarrier(TextureMemoryBarrierOptions{
+            .srcStages = PipelineStageFlagBit::TransferBit,
+            .srcMask = AccessFlagBit::None,
+            .dstStages = PipelineStageFlagBit::TransferBit,
+            .dstMask = AccessFlagBit::TransferWriteBit,
+            .oldLayout = TextureLayout::Undefined,
+            .newLayout = TextureLayout::TransferDstOptimal,
+            .texture = options.texture,
+            .range = {
+                    .aspectMask = TextureAspectFlagBits::ColorBit,
+                    .baseMipLevel = 1,
+                    .levelCount = options.mipLevels - 1,
+            },
+    });
 
-        // Transition miplevel to TransforDstOptimal
+    auto blitOptions = TextureBlitOptions{
+            .srcTexture = options.texture,
+            .srcLayout = TextureLayout::TransferSrcOptimal,
+            .dstTexture = options.texture,
+            .dstLayout = TextureLayout::TransferDstOptimal,
+            .regions = {
+                    {
+                            .srcSubresource = {
+                                    .aspectMask = TextureAspectFlagBits::ColorBit,
+                            },
+                            .dstSubresource = {
+                                    .aspectMask = TextureAspectFlagBits::ColorBit,
+                            },
+                    },
+            },
+            .scalingFilter = FilterMode::Linear,
+    };
+
+    uint32_t prevLevelWidth = options.extent.width;
+    uint32_t prevLevelHeight = options.extent.height;
+    uint32_t prevLevelDepth = options.extent.depth;
+    for (uint32_t mipLevel = 1; mipLevel < options.mipLevels; ++mipLevel) {
+        const uint32_t levelWidth = std::max(prevLevelWidth >> 1, 1u);
+        const uint32_t levelHeight = std::max(prevLevelHeight >> 1, 1u);
+        const uint32_t levelDepth = std::max(prevLevelDepth >> 1, 1u);
+
+        TextureBlitRegion &region = blitOptions.regions[0];
+        region.srcSubresource.mipLevel = mipLevel - 1;
+        region.srcExtent = {
+                .width = prevLevelWidth,
+                .height = prevLevelHeight,
+                .depth = 1,
+        };
+        region.dstSubresource.mipLevel = mipLevel;
+        region.dstExtent = {
+                .width = levelWidth,
+                .height = levelHeight,
+                .depth = 1,
+        };
+
+        // Blit previous miplevel into current miplevel
+        for (uint32_t layer = 0; layer < options.layerCount; ++layer) {
+            for (uint32_t slice = 0; slice < levelDepth; ++slice) {
+                region.srcSubresource.baseArrayLayer = layer;
+                region.srcOffset.z = static_cast<int32_t>(
+                    prevLevelDepth > levelDepth ? slice << 1 : slice);
+                region.dstSubresource.baseArrayLayer = layer;
+                region.dstOffset.z = static_cast<int32_t>(slice);
+                blitTexture(blitOptions);
+            }
+        }
+
+        // Transition miplevel to TransferSrcOptimal
         textureMemoryBarrier(TextureMemoryBarrierOptions{
                 .srcStages = PipelineStageFlagBit::TransferBit,
-                .srcMask = AccessFlagBit::None,
+                .srcMask = AccessFlagBit::TransferWriteBit,
                 .dstStages = PipelineStageFlagBit::TransferBit,
-                .dstMask = AccessFlagBit::TransferWriteBit,
-                .oldLayout = TextureLayout::Undefined,
-                .newLayout = TextureLayout::TransferDstOptimal,
+                .dstMask = AccessFlagBit::TransferReadBit,
+                .oldLayout = TextureLayout::TransferDstOptimal,
+                .newLayout = TextureLayout::TransferSrcOptimal,
                 .texture = options.texture,
                 .range = {
                         .aspectMask = TextureAspectFlagBits::ColorBit,
@@ -568,67 +634,12 @@ void VulkanCommandRecorder::generateMipMaps(const GenerateMipMapsOptions &option
                 },
         });
 
-        // Blit main mip level into sub level
-        blitTexture(TextureBlitOptions{
-                .srcTexture = options.texture,
-                .srcLayout = TextureLayout::TransferSrcOptimal,
-                .dstTexture = options.texture,
-                .dstLayout = TextureLayout::TransferDstOptimal,
-                .regions = {
-                        {
-                                .srcSubresource = {
-                                        .aspectMask = TextureAspectFlagBits::ColorBit,
-                                        .mipLevel = 0,
-                                },
-                                .srcOffset = {
-                                        .x = 0,
-                                        .y = 0,
-                                        .z = 0,
-                                },
-                                .srcExtent = {
-                                        .width = options.extent.width,
-                                        .height = options.extent.height,
-                                        .depth = 1,
-                                },
-                                .dstSubresource = {
-                                        .aspectMask = TextureAspectFlagBits::ColorBit,
-                                        .mipLevel = mipLevel,
-                                },
-                                .dstOffset = {
-                                        .x = 0,
-                                        .y = 0,
-                                        .z = 0,
-                                },
-                                .dstExtent = {
-                                        .width = (options.extent.width >> mipLevel),
-                                        .height = (options.extent.height >> mipLevel),
-                                        .depth = 1,
-                                },
-                        },
-                },
-                .scalingFilter = FilterMode::Linear,
-        });
-
-        // Transition miplevel to newLayout
-        if (options.newLayout != TextureLayout::TransferDstOptimal && 
-                options.newLayout != TextureLayout::Undefined)
-            textureMemoryBarrier(TextureMemoryBarrierOptions{
-                    .srcStages = PipelineStageFlagBit::TransferBit,
-                    .srcMask = AccessFlagBit::TransferWriteBit,
-                    .dstStages = PipelineStageFlagBit::TransferBit,
-                    .dstMask = AccessFlagBit::TransferReadBit,
-                    .oldLayout = TextureLayout::TransferDstOptimal,
-                    .newLayout = options.newLayout,
-                    .texture = options.texture,
-                    .range = {
-                            .aspectMask = TextureAspectFlagBits::ColorBit,
-                            .baseMipLevel = mipLevel,
-                            .levelCount = 1,
-                    },
-            });
+        prevLevelWidth = levelWidth;
+        prevLevelHeight = levelHeight;
+        prevLevelDepth = levelDepth;
     }
 
-    // Transition base miplevel to newLayout
+    // Transition all miplevels to newLayout
     if (options.newLayout != TextureLayout::TransferSrcOptimal && 
             options.newLayout != TextureLayout::Undefined)
         textureMemoryBarrier(TextureMemoryBarrierOptions{
@@ -642,7 +653,7 @@ void VulkanCommandRecorder::generateMipMaps(const GenerateMipMapsOptions &option
                 .range = {
                         .aspectMask = TextureAspectFlagBits::ColorBit,
                         .baseMipLevel = 0,
-                        .levelCount = 1,
+                        .levelCount = options.mipLevels,
                 },
         });
 }
